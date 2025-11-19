91e55f8582565ee17f2b4c84d748bf419a2de4c5
Syed Saddam Shah <saddamshah415@gmail.com>
2025-11-18 20:18:15 -0500

    Frontend Registration Page Created

--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from './Button.jsx'
 
+
 const Main = () => {
   return (
     <>
@@ -9,8 +10,6 @@ const Main = () => {
         <h1 className = 'text-light'>Stock Prediction App</h1>
         <p className = 'text-light lead'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum aliquid possimus totam porro mollitia natus doloremque sed vel, quos amet labore beatae eum impedit accusantium ipsum corporis recusandae qui aperiam doloribus accusamus obcaecati laboriosam ipsam. Aut facilis maiores, explicabo veritatis quos aperiam temporibus doloribus, nihil repellendus vitae esse eius illo?</p>
         <Button text = 'Login' class = 'btn-outline-info'/>
-
-
       </div>
     </div>
     </>
---
+++ frontend-react/src/components/Register.jsx
@@ -0,0 +1,49 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+
+const Register = () => {
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleRegistration = async (e) => {
+        e.preventDefault();
+
+        const userData = {
+        username, email, password            
+    }
+
+    try{
+        const response = await axios.post('http://127.0.0.1:8000/api/v1/register/', userData)
+        console.log('response.data==>' ,response.data)
+        console.log('Registration Successful')
+
+    }catch(error){
+        console.error('Registration Error:' , error.response.data)
+
+    }
+    
+
+    
+    }
+
+  return (
+    <>
+    <div className = 'container'>
+        <div className="row justify-content-center">
+            <div className="col-md-6 bg-l ight-dark p-5 rounded">
+                <h3 className="text-light text-center mb-4"> Create an Acoount</h3>
+                <form onSubmit={handleRegistration}>
+                    <input type="text" className="form-control mb-3" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)}/>
+                    <input type="email" className="form-control mb-3" placeholder='Email Address' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <input type="password" className="form-control mb-5" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <button type='submit' className='btn btn-info d-block mx-auto'> Register</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Register
