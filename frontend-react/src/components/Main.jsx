cf6bee3b5e01ebb3d938b2638b1ee5f866254ad2
Syed Saddam Shah <saddamshah415@gmail.com>
2025-11-25 19:27:08 -0500

    authentication functionalities

--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -9,7 +9,7 @@ const Main = () => {
       <div className = 'p-5 text-center bg-light-dark rounded'>
         <h1 className = 'text-light'>Stock Prediction App</h1>
         <p className = 'text-light lead'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum aliquid possimus totam porro mollitia natus doloremque sed vel, quos amet labore beatae eum impedit accusantium ipsum corporis recusandae qui aperiam doloribus accusamus obcaecati laboriosam ipsam. Aut facilis maiores, explicabo veritatis quos aperiam temporibus doloribus, nihil repellendus vitae esse eius illo?</p>
-        <Button text = 'Login' class = 'btn-outline-info'/>
+        <Button text = 'Explore Now' class = 'btn-outline-info' url = '/dashboard'/>
       </div>
     </div>
     </>
